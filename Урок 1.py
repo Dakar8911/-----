ae8014b053f8ea0b35011e2560b8dedd7bf08e46
Dakar8911 <limarzahar@gmail.com>
2023-05-31 22:35:49 +0800

    создал гид

---
+++ Урок 1.py
@@ -0,0 +1,29 @@
+a = 3
+b = 5
+c = 2
+print(a+b-c+(a+b/c)) # мама помогите
+
+print(True == False)
+
+вадим = 3
+влад = 15
+print(влад*вадим)
+
+y = 'Салам провославный'
+print(y,type(y))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(int(a/b))
+
+G = 'Император'
+F = 'Бог'
+print(F+' '+G)
+
+Y = (0,1,2,3,4,5,6,7,8,9)
+N = [0,1,2,3,4,5,6,7,8,9]
+
+print(Y,type(Y))
+print(Y,type(N))
